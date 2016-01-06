aa3aa69cd84220a1653974cb9d7f91f681ea6c94
imoran <imoran@mail.ccsf.edu>
2016-01-05 22:06:54 -0800

    first commit

---
+++ MovieViewer/MovieCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieCell.swift
+//  MovieViewer
+//
+//  Created by Isis  on 1/5/16.
+//  Copyright © 2016 codepath. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
