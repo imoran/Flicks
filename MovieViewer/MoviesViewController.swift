95b7c97e1b7bb0377b38fb3ea64bb3bd73108d7f
imoran <imoran@mail.ccsf.edu>
2016-01-10 22:05:41 -0800

    Added refresh control

--- MovieViewer/MoviesViewController.swift
+++ MovieViewer/MoviesViewController.swift
@@ -9,20 +9,44 @@
 import UIKit
 import AFNetworking
 
-class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
-    @IBOutlet weak var tableView: UITableView!
+class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate {
     
+    var refreshControl: UIRefreshControl!
+    let delay = 3.0 * Double(NSEC_PER_SEC)
     var movies: [NSDictionary]?
+    let indicator: UIActivityIndicatorView = UIActivityIndicatorView(activityIndicatorStyle: UIActivityIndicatorViewStyle.Gray)
+    
+    var searchActive: Bool = false
+    var filteredData: [String]!
+    
+
+    @IBOutlet weak var movieSearch: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+        indicator.frame = CGRect(x: 0.0, y: 0.0, width: 40.0, height: 40.0)
+        refreshControl = UIRefreshControl()
+        tableView.addSubview(refreshControl)
+        tableView.addSubview(indicator)
+        indicator.startAnimating()
+        
+//        refreshControl.backgroundColor = UIColor.grayColor()
+//        refreshControl.tintColor = UIColor.blackColor()
+        
+        refreshControl.addTarget(self, action: "onRefresh", forControlEvents: UIControlEvents.ValueChanged)
+
         tableView.dataSource = self
         tableView.delegate = self
+        indicator.center = tableView.center
+        
+//        movieSearch.delegate = self
         
         let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
-        let url = NSURL(string:"https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")
+        let url = NSURL(string:"http://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")
         let request = NSURLRequest(URL: url!)
         let session = NSURLSession(
             configuration: NSURLSessionConfiguration.defaultSessionConfiguration(),
@@ -37,8 +61,9 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
                         data, options:[]) as? NSDictionary {
                             NSLog("response: \(responseDictionary)")
                             
-                            self.movies = responseDictionary["results"] as! [NSDictionary]
+                            self.movies = responseDictionary["results"] as? [NSDictionary]
                             self.tableView.reloadData()
+//                            self.indicator.stopAnimating()
                             
                     }
                 }
@@ -46,50 +71,63 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         task.resume()
 
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
-        if let movies = movies {
-            return movies.count
-        } else {
-           return 0
+    
+    func delay(delay:Double, closure:() -> ()) {
+        dispatch_after(
+         dispatch_time(
+          DISPATCH_TIME_NOW,
+          Int64(delay * Double(NSEC_PER_SEC))
+        ),
+        dispatch_get_main_queue(), closure)
     }
-        
-}
+    
+    func onRefresh() {
+        delay(2, closure: {
+            self.refreshControl.endRefreshing()
+        })
+    }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("MovieCell", forIndexPath: indexPath) as! MovieCell
         
         let movie = movies![indexPath.row]
         let title = movie["title"] as! String
-        let overview = movie["overview"] as! String
+        let overview = movie["overview"]
         let posterPath = movie["poster_path"] as! String
         
         let baseUrl = "http://image.tmdb.org/t/p/w500"
-        
         let imageUrl = NSURL(string: baseUrl + posterPath)
         
-        cell.titleLabel.text = title
-        cell.overviewLabel.text = overview
-        cell.posterView.setImageWithURL(imageUrl!)
+        cell.titleLabel.text = title as String
+        cell.overviewLabel.text = overview as? String
         
+        if let posterPath = movie["poster_path"] as? String {
+            cell.posterView.setImageWithURL(imageUrl!)
+        }
         
+        return cell
         
         print("row\(indexPath.row)")
         return cell
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
     }
-    */
-
+    
+    
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        if let movies = movies {
+            return movies.count
+        } else {
+            return 0
+        
+    }
   }
+}
